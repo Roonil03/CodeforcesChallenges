e20855f1d9a43eea88348dfd8ae2d98e21826490
Roonil03 <aryanvivek05@gmail.com>
2026-07-15 23:22:02 +0530

    contest 1109 div 3 (unrated contest) information

---
+++ Round1109(Div3)_Unrated/F. AnyaLovesTrees!/anyaLovesTrees.go
@@ -0,0 +1,259 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"math"
+	"os"
+)
+
+var (
+	in  = NewFastScanner()
+	out = bufio.NewWriter(os.Stdout)
+)
+
+type FastScanner struct {
+	r *bufio.Reader
+}
+
+func NewFastScanner() *FastScanner {
+	return &FastScanner{r: bufio.NewReaderSize(os.Stdin, 1<<20)}
+}
+
+func (fs *FastScanner) rInt() int {
+	sign, val := 1, 0
+	c, err := fs.r.ReadByte()
+	for (c < '0' || c > '9') && c != '-' {
+		if err != nil {
+			return 0
+		}
+		c, err = fs.r.ReadByte()
+	}
+	if c == '-' {
+		sign = -1
+		c, _ = fs.r.ReadByte()
+	}
+	for c >= '0' && c <= '9' {
+		val = val*10 + int(c-'0')
+		c, err = fs.r.ReadByte()
+		if err != nil {
+			break
+		}
+	}
+	return sign * val
+}
+
+func (fs *FastScanner) rInt64() int64 {
+	sign, val := int64(1), int64(0)
+	c, err := fs.r.ReadByte()
+	for (c < '0' || c > '9') && c != '-' {
+		if err != nil {
+			return 0
+		}
+		c, err = fs.r.ReadByte()
+	}
+	if c == '-' {
+		sign = -1
+		c, _ = fs.r.ReadByte()
+	}
+	for c >= '0' && c <= '9' {
+		val = val*10 + int64(c-'0')
+		c, err = fs.r.ReadByte()
+		if err != nil {
+			break
+		}
+	}
+	return sign * val
+}
+
+func (fs *FastScanner) rString() string {
+	buf := make([]byte, 0, 16)
+	c, err := fs.r.ReadByte()
+	for c == ' ' || c == '\n' || c == '\r' || c == '\t' {
+		if err != nil {
+			return ""
+		}
+		c, err = fs.r.ReadByte()
+	}
+	for c != ' ' && c != '\n' && c != '\r' && c != '\t' {
+		buf = append(buf, c)
+		c, err = fs.r.ReadByte()
+		if err != nil {
+			break
+		}
+	}
+	return string(buf)
+}
+
+func max64(a, b int64) int64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxf32(a, b float32) float32 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func maxf64(a, b float64) float64 {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+func min64(a, b int64) int64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minf32(a, b float32) float32 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func minf64(a, b float64) float64 {
+	if a < b {
+		return a
+	}
+	return b
+}
+
+func abs64(a int64) int64 {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func absf32(a float32) float32 {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func absf64(a float64) float64 {
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func readInts(n int) []int {
+	a := make([]int, n)
+	for i := 0; i < n; i++ {
+		a[i] = in.rInt()
+	}
+	return a
+}
+
+func readInt64s(n int) []int64 {
+	a := make([]int64, n)
+	for i := 0; i < n; i++ {
+		a[i] = in.rInt64()
+	}
+	return a
+}
+
+func printSlice(arr []int) {
+	for i, v := range arr {
+		if i > 0 {
+			fmt.Fprint(out, " ")
+		}
+		fmt.Fprint(out, v)
+	}
+	fmt.Fprintln(out)
+}
+
+func gcd(a, b int) int {
+	for b != 0 {
+		a, b = b, a%b
+	}
+	if a < 0 {
+		return -a
+	}
+	return a
+}
+
+func lcm(a, b int) int {
+	return a / gcd(a, b) * b
+}
+
+func precomp() {
+
+}
+
+func solve() {
+	n := in.rInt()
+	// arr := readInts(n)
+	adj := make([][]int, n+1)
+	for i := 2; i <= n; i++ {
+		p := in.rInt()
+		adj[p] = append(adj[p], i)
+	}
+	arr := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		arr[i] = in.rInt()
+	}
+	var dfs func(u int) (int, int, int, bool)
+	dfs = func(u int) (int, int, int, bool) {
+		if len(adj[u]) == 0 {
+			return arr[u], arr[u], 1, true
+		}
+		a, b, sz := math.MaxInt, math.MinInt, 0
+		m := make([]int, 0, len(adj[u]))
+		for _, v := range adj[u] {
+			c1, c2, csz, fg := dfs(v)
+			if !fg {
+				return 0, 0, 0, false
+			}
+			a = min(a, c1)
+			b = max(b, c2)
+			sz += csz
+			m = append(m, c1)
+		}
+		if b-a+1 != sz {
+			return 0, 0, 0, false
+		}
+		t := 0
+		for i := range len(m) - 1 {
+			if m[i] > m[i+1] {
+				t++
+			}
+		}
+		if t > 1 {
+			return 0, 0, 0, false
+		}
+		if t == 1 && m[len(m)-1] > m[0] {
+			return 0, 0, 0, false
+		}
+		return a, b, sz, true
+	}
+	_, _, _, fg := dfs(1)
+	if fg {
+		fmt.Fprintln(out, "yes")
+	} else {
+		fmt.Fprintln(out, "no")
+	}
+	// printSlice(arr)
+}
+
+func main() {
+	out = bufio.NewWriterSize(os.Stdout, 1<<20)
+	defer out.Flush()
+	precomp()
+	t := in.rInt()
+	for t > 0 {
+		t--
+		solve()
+	}
+}
---
+++ Round1109(Div3)_Unrated/README.md
@@ -0,0 +1,8 @@
+# Codeforces Round 1109 (Div. 3)
+
+## Unrated Participation
+### Questions Solved: 6/7
+### Questions Attempted: 6/7
+
+The contest was conducted on 14th July, 2026 from 14:35 UTC  
+The contest can be referred from [here](https://codeforces.com/contest/2244)
